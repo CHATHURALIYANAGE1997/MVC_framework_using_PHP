0e67cda1b65dfc20579ff3b19c0834f00d0a1292
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-04-19 00:45:25 +0530

    Update config.php

--- mvc_framework/src/core/config.php
+++ mvc_framework/src/core/config.php
@@ -1,13 +1,13 @@
 <?php
 
 
-define('PATH', 'http://localhost/mvc');
-define('CSS_PATH', 'http://localhost/mvc/public/css/');
-define('IMG_PATH', 'http://localhost/mvc/public/img/');
-define('DOC_PATH', 'http://localhost/mvc/public/documents/');
+define('PATH', 'http://localhost/mvc_framework');
+define('CSS_PATH', 'http://localhost/mvc_framework/public/css/');
+define('IMG_PATH', 'http://localhost/mvc_framework/public/img/');
+define('DOC_PATH', 'http://localhost/mvc_framework/public/documents/');
 
 
-define('DBNAME', 'mvc');
+define('DBNAME', 'db_name');
 define('DBHOST', 'localhost');
 define('DBUSER', 'root');
 define('DBPASS', '');
