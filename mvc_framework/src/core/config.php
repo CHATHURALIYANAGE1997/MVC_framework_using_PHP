ec0444d3f2416c927cd62163aee0cf4ff89afc37
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-04-19 00:42:31 +0530

    Add files via upload

---
+++ mvc_framework/src/core/config.php
@@ -0,0 +1,15 @@
+<?php
+
+
+define('PATH', 'http://localhost/mvc');
+define('CSS_PATH', 'http://localhost/mvc/public/css/');
+define('IMG_PATH', 'http://localhost/mvc/public/img/');
+define('DOC_PATH', 'http://localhost/mvc/public/documents/');
+
+
+define('DBNAME', 'mvc');
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBDRIVER', 'mysql');
+
